06933fa322f9977a19c27006731719dd18ca7772
yoserepelin <joseocampo1015@hotmail.com>
2024-11-05 11:20:37 -0500

    First Commit

---
+++ CyberGaming/CyberGaming/firebaseconect.js
@@ -0,0 +1,56 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDnlJROo1NeoEECfi9HBs1tYHpSKstXZ3c",
+  authDomain: "login-registro-cg.firebaseapp.com",
+  projectId: "login-registro-cg",
+  storageBucket: "login-registro-cg.appspot.com",
+  messagingSenderId: "944833409561",
+  appId: "1:944833409561:web:a8d992001463f0fe377088"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+
+export class ManageAccount {
+  register(email, password) {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((_) => {
+        window.location.href = "Login.html";
+        // Mostrar alerta de registro exitoso
+        alert("Registro exitoso. Serás redirigido a la página de inicio de sesión.");
+      })
+      .catch((error) => {
+        console.error(error.message);
+            // Mostrar alerta de error de registro
+            alert("Error al registrar: " + error.message);
+      });
+  }
+
+  authenticate(email, password) {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((_) => {
+        window.location.href = "home.html";
+        // Mostrar alerta de inicio de sesión exitoso
+        alert("Has iniciado sesión correctamente. Serás redirigido a la página principal.");
+      })
+      .catch((error) => {
+        console.error(error.message);
+                // Mostrar alerta de error de inicio de sesión
+                alert("Error al iniciar sesión: " + error.message);
+      });
+  }
+
+  signOut() {
+    signOut(auth)
+      .then((_) => {
+        window.location.href = "Login.html";
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  }
+}
